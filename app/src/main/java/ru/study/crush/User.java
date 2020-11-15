419061d168f50e1365e873dbc9c83d5470945738
Crush <art60@rambler.ru>
2020-11-15 17:59:22 +0300

    Add model

---
+++ app/src/main/java/ru/study/crush/User.java
@@ -0,0 +1,49 @@
+package ru.study.crush;
+
+public class User {
+    private int id;
+    private String name;
+    private int age;
+    private boolean isLogged;
+    private Position position;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean isLogged() {
+        return isLogged;
+    }
+
+    public void setLogged(boolean logged) {
+        isLogged = logged;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    public void setPosition(Position position) {
+        this.position = position;
+    }
+}
