c758f9f23b70c0fdd3208b4d5764e9e138809195
Crush <art60@rambler.ru>
2020-11-15 17:32:04 +0300

    Начальный коммит

---
+++ app/src/main/java/ru/study/crush/MainFragment.java
@@ -0,0 +1,25 @@
+package ru.study.crush;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class MainFragment extends Fragment {
+    public static Fragment newInstance() {
+        return new MainFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
